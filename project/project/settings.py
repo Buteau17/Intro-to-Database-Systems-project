7119e17e5188c2d353ac960d515edb3f9bbc8d5f
Buteau17 <buteauwillens@gmail.com>
2023-06-11 21:39:37 +0800

    a simple graph

--- project/project/settings.py
+++ project/project/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [ './carAccident'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
