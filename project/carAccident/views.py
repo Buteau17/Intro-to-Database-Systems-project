6b0c58c944731be333d7795e8ee3cff914179da6
Buteau17 <buteauwillens@gmail.com>
2023-06-11 22:20:02 +0800

    add two simple graphs

--- project/carAccident/views.py
+++ project/carAccident/views.py
@@ -4,7 +4,7 @@ from plotly.graph_objs import Scatter
 
 # Create your views here.
 from django.http import HttpResponse
-
+import plotly.express as px
 
 def index(request):
     x_data = [0,1,2,3]
@@ -13,4 +13,11 @@ def index(request):
                         mode='lines', name='test',
                         opacity=0.8, marker_color='green')],
                output_type='div')
-    return render(request, "index.html", context={'plot_div': plot_div})
+
+    x = ['rainy', 'sunny', 'snow']
+    y = [52, 104, 33]
+    df = px.data.gapminder().query("year == 2007").query("continent == 'Europe'")
+    df.loc[df['pop'] < 2.e6, 'country'] = 'Other countries' # Represent only large countries
+    fig = px.pie(df, values='pop', names='country', title='Population of European continent')
+    plot2_div = plot(fig, output_type='div')
+    return render(request, "index.html", context={'plot_div': plot_div,'a_div': plot2_div })
