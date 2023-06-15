305fc27eeaa7b5b4c528c3b293497891a122e361
Buteau17 <buteauwillens@gmail.com>
2023-06-15 23:04:14 +0800

    a little modification

--- project/carAccident/views.py
+++ project/carAccident/views.py
@@ -45,33 +45,31 @@ def destroy(request, id):
     return redirect("/show")  
 
 def index(request):
-    x_data = list(AmountAccidentbymonth().keys())
-    y_data = list(AmountAccidentbymonth().values())
-    plot_div = plot([Scatter(x=x_data, y=y_data,
-                        mode='lines', name='test',
-                        opacity=0.8, marker_color='green')],
-               output_type='div')
+    df = pd.DataFrame(list(zip(list(AmountAccidentbymonth().keys()),list(AmountAccidentbymonth().values()))), columns=['month', 'count'])
+    print(df)
+    fig = px.line(df, y='count', x='month', title='Amount of accident per month (2022)')
+    plot_div = plot(fig, output_type='div')
 
     x = ['rainy', 'sunny', 'snow']
     y = [52, 104, 33]
     df = pd.DataFrame(list(zip(list(GetDeathByGender().keys()),list(GetDeathByGender().values()))), columns=['gender', 'count'])
     print(df)
-    fig = px.pie(df, values='count', names='gender', title='Death by gender')
+    fig = px.pie(df, values='count', names='gender', title='Death by gender(2022)')
     plot2_div = plot(fig, output_type='div')
      
     df = pd.DataFrame(list(zip(list(Weather().keys()),list(Weather().values()))), columns=['weather', 'count'])
     print(df)
-    fig = px.pie(df, values='count', names='weather', title='Percentage of weather conditions under which accidents happened')
+    fig = px.pie(df, values='count', names='weather', title='Percentage of weather conditions under which accidents happened(2022)')
     plot3_div = plot(fig, output_type='div')
 
     df = pd.DataFrame(list(zip(list(GetLevelofinjurybygender()['male'].keys()),list(GetLevelofinjurybygender()['male'].values()))), columns=['male', 'count'])
     print(df)
-    fig = px.pie(df, values='count', names='male', title='Level of injury for male')
+    fig = px.pie(df, values='count', names='male', title='Level of injury for male (2022)')
     plot4_div = plot(fig, output_type='div')
 
     df = pd.DataFrame(list(zip(list(GetLevelofinjurybygender()['female'].keys()),list(GetLevelofinjurybygender()['female'].values()))), columns=['female', 'count'])
     print(df)
-    fig = px.pie(df, values='count', names='female', title='Level of injury for female')
+    fig = px.pie(df, values='count', names='female', title='Level of injury for female (2022)')
     plot5_div = plot(fig, output_type='div')
     return render(request, "home.html", context={'plot_div': plot_div,'a_div': plot2_div,'b_div': plot3_div , 'c_div': plot4_div, 'd_div': plot5_div})
 
